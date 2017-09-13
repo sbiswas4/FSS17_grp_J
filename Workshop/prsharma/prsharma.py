10beaa93a00a772050ac3e10f8aa41483b8e4dce
pranita-s <pranitarsharma@gmail.com>
2017-09-12 21:29:00 -0400

    upload

---
+++ Workshop/prsharma/prsharma.py
@@ -0,0 +1,194 @@
+
+""" Part 1 """
+# Load data
+import numpy as np
+from sklearn import datasets
+iris = datasets.load_iris()
+iris_X = iris.data
+iris_y = iris.target
+
+print(iris.feature_names)
+print(iris.target_names)
+print(np.unique(iris_y))
+
+
+
+# Visualize data
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+X = iris.data[:, :2]  # we only take the first two features.
+y = iris.target
+x_min, x_max = X[:, 0].min() - .5, X[:, 0].max() + .5
+y_min, y_max = X[:, 1].min() - .5, X[:, 1].max() + .5
+
+
+
+# Plot the training points
+plt.figure(2, figsize=(8, 6))
+plt.clf()
+plt.scatter(X[:, 0], X[:, 1], c=y, cmap=plt.cm.Set1, edgecolor='k')
+plt.xlabel('Sepal length')
+plt.ylabel('Sepal width')
+plt.xlim(x_min, x_max)
+plt.ylim(y_min, y_max)
+plt.xticks(())
+plt.yticks(())
+plt.show()
+
+
+
+# Plot the distribution in boxplot
+plt.boxplot(iris.data)
+plt.xlabel('Features')
+plt.ylabel('Cm')
+plt.show()
+
+
+""" Part 2 """
+# KNN Classification
+# shuffle the data
+np.random.seed(0)
+indices = np.random.permutation(len(iris_X))
+# Split iris data in train and test data
+# A random permutation, to split the data randomly
+iris_X_train = iris_X[indices[:-10]]
+iris_y_train = iris_y[indices[:-10]]
+iris_X_test  = iris_X[indices[-10:]]
+iris_y_test  = iris_y[indices[-10:]]
+
+# Create and fit a nearest-neighbor classifier
+from sklearn.neighbors import KNeighborsClassifier
+knn = KNeighborsClassifier()
+knn.fit(iris_X_train, iris_y_train) 
+
+# Do prediction on the test data
+knn.predict(iris_X_test)
+print iris_y_test
+
+
+
+# KNN Visualization.
+n_neighbors = 30
+neighbors = [5,15,30]
+
+h = .02  # step size in the mesh
+
+for n_neighbors in neighbors:
+
+# Create color maps
+    from matplotlib.colors import ListedColormap
+    cmap_light = ListedColormap(['#FFAAAA', '#AAFFAA', '#AAAAFF'])
+    cmap_bold = ListedColormap(['#FF0000', '#00FF00', '#0000FF'])
+
+    for weights in ['uniform', 'distance']:
+    # we create an instance of Neighbours Classifier and fit the data.
+        knn = KNeighborsClassifier(n_neighbors, weights=weights)
+        knn.fit(X, y)
+
+    # Plot the decision boundary. For that, we will assign a color to each
+    # point in the mesh [x_min, x_max]x[y_min, y_max].
+        xx, yy = np.meshgrid(np.arange(x_min, x_max, h),
+                         np.arange(y_min, y_max, h))
+        Z = knn.predict(np.c_[xx.ravel(), yy.ravel()])
+
+    # Put the result into a color plot
+        Z = Z.reshape(xx.shape)
+        plt.figure()
+        plt.pcolormesh(xx, yy, Z, cmap=cmap_light)
+
+    # Plot also the training points
+        plt.scatter(X[:, 0], X[:, 1], c=y, cmap=cmap_bold,
+                edgecolor='k', s=20)
+        plt.xlim(xx.min(), xx.max())
+        plt.ylim(yy.min(), yy.max())
+        plt.title("3-Class classification (k = %i, weights = '%s')"
+              % (n_neighbors, weights))
+
+plt.show()
+
+
+'''Part 3'''
+
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn import svm, datasets
+
+
+def make_meshgrid(x, y, h=.02):
+    """Create a mesh of points to plot in
+
+    Parameters
+    ----------
+    x: data to base x-axis meshgrid on
+    y: data to base y-axis meshgrid on
+    h: stepsize for meshgrid, optional
+
+    Returns
+    -------
+    xx, yy : ndarray
+    """
+    x_min, x_max = x.min() - 1, x.max() + 1
+    y_min, y_max = y.min() - 1, y.max() + 1
+    xx, yy = np.meshgrid(np.arange(x_min, x_max, h),
+                         np.arange(y_min, y_max, h))
+    return xx, yy
+
+
+def plot_contours(ax, clf, xx, yy, **params):
+    """Plot the decision boundaries for a classifier.
+
+    Parameters
+    ----------
+    ax: matplotlib axes object
+    clf: a classifier
+    xx: meshgrid ndarray
+    yy: meshgrid ndarray
+    params: dictionary of params to pass to contourf, optional
+    """
+    Z = clf.predict(np.c_[xx.ravel(), yy.ravel()])
+    Z = Z.reshape(xx.shape)
+    out = ax.contourf(xx, yy, Z, **params)
+    return out
+
+
+# import some data to play with
+iris = datasets.load_iris()
+# Take the first two features. We could avoid this by using a two-dim dataset
+X = iris.data[:, :2]
+y = iris.target
+
+# we create an instance of SVM and fit out data. We do not scale our
+# data since we want to plot the support vectors
+C = 1.0  # SVM regularization parameter
+models = (svm.SVC(kernel='linear', C=C),
+          svm.LinearSVC(C=C),
+          svm.SVC(kernel='rbf', gamma=0.7, C=C),
+          svm.SVC(kernel='poly', degree=3, C=C))
+models = (clf.fit(X, y) for clf in models)
+
+# title for the plots
+titles = ('SVC with linear kernel',
+          'LinearSVC (linear kernel)',
+          'SVC with RBF kernel',
+          'SVC with polynomial (degree 3) kernel')
+
+# Set-up 2x2 grid for plotting.
+fig, sub = plt.subplots(2, 2)
+plt.subplots_adjust(wspace=0.4, hspace=0.4)
+
+X0, X1 = X[:, 0], X[:, 1]
+xx, yy = make_meshgrid(X0, X1)
+
+for clf, title, ax in zip(models, titles, sub.flatten()):
+    plot_contours(ax, clf, xx, yy,
+                  cmap=plt.cm.coolwarm, alpha=0.8)
+    ax.scatter(X0, X1, c=y, cmap=plt.cm.coolwarm, s=20, edgecolors='k')
+    ax.set_xlim(xx.min(), xx.max())
+    ax.set_ylim(yy.min(), yy.max())
+    ax.set_xlabel('Sepal length')
+    ax.set_ylabel('Sepal width')
+    ax.set_xticks(())
+    ax.set_yticks(())
+    ax.set_title(title)
+
+plt.show()
